9cc98c48a822890dd2b3839e48113519d338debd
Chinenyem <chnw1947@gmail.com>
2016-12-03 20:15:18 -0500

    edit comments and flag comments

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -14,5 +14,31 @@ describe('actions', () => {
 
       expect(actions.updateScheduledItem(itemId, args)).toEqual(expectedAction);
     });
+    it('should create an action to edit a service item', () => {
+      const itemId = 1;
+      const args = { comment: '' };
+      const expectedAction = {
+        args,
+        itemId,
+        type: types.EDIT_TECH_COMMENT,
+      };
+
+      expect(actions.editTechComment(itemId, args)).toEqual(expectedAction);
+    });
+
+    it('should create an action to flag a service item', () => {
+      const itemId = 1;
+      const args = { flag: true };
+      const expectedAction = {
+        args,
+        itemId,
+        type: types.FLAG_COMMENT,
+      };
+
+      expect(actions.flagComment(itemId, args)).toEqual(expectedAction);
+    });
+
+
+
   });
 });
--- src/containers/Comments.js
+++ src/containers/Comments.js
@@ -2,11 +2,16 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import map from 'lodash/map';
 import Comment from '../components/Comment';
+import { editTechComment } from '../actions';
+import { flagComment } from '../actions';
+
 
 class Comments extends Component {
   render() {
     const {
       className,
+      editTechComment,
+      flagComment,
       items,
     } = this.props;
 
@@ -14,9 +19,17 @@ class Comments extends Component {
       <div className={`${className} Comments`}>
         <h3>Comments</h3>
         <ul>
-          { map(items, item => <Comment item={item} key={item.id} />) }
+          { map(items, item =>
+            <Comment
+            clickHandler={editTechComment.bind(null, item)}
+            toggleHandler={flagComment.bind(null, item)}
+            item={item}
+            key={item.id}
+            />)
+          }
         </ul>
       </div>
+
     );
   };
 }
@@ -26,9 +39,18 @@ const mapStateToProps = (state, ownProps) => ({
   items: state.comments,
 });
 
+const mapDispatchToProps = (dispatch) => ({
+  editTechComment: (item) => {
+    dispatch(editTechComment(item.id, { comment: item.body  }));
+  },
+  flagComment: (item) => {
+    dispatch(flagComment(item.id, { flagged: item.flagged  }));
+  },
+});
+
 Comments.propTypes = {
   className: PropTypes.string,
   items: PropTypes.object.isRequired,
 };
 
-export default connect(mapStateToProps)(Comments);
+export default connect(mapStateToProps, mapDispatchToProps)(Comments);
